bd55e335528b231dc1df20a7d99009842d296f7c
unknown <bagusrizkyr@mail.ugm.ac.id>
2023-11-30 12:47:29 +0700

    change requirement

--- code/1_Home.py
+++ code/1_Home.py
@@ -1,5 +1,5 @@
 import pandas as pd #pip install pandas openpyxl
-#import plotly.express as px # pip install plotly-express
+import plotly.express as px # pip install plotly-express
 import streamlit as st #pip install streamlit
 import openpyxl #pip install openpyxl
 
